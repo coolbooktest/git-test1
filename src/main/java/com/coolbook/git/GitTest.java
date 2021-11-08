c008f001c8854fb34faebab40fe3bc9dfbff2645
coolbook <1829979023@qq.com>
2021-11-08 14:10:29 +0800

    first commit

---
+++ src/main/java/com/coolbook/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.coolbook.git;
+
+/**
+ * @author coolbook
+ * @version 1.0
+ * @ClassName
+ * @Decription
+ * @creat 2021-11-08-13:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("github");
+    }
+}
